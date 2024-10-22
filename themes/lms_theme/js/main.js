b105cfe44d0e21a495def18ed91f7f3dbf455b3c
Phat Le Huu <phat.le-huu@weebpal.com>
2024-10-22 10:40:45 +0700

    Update js and install permission

--- themes/lms_theme/js/main.js
+++ themes/lms_theme/js/main.js
@@ -5,9 +5,19 @@
   Drupal.behaviors.main = {
 	 attach: function (context) {
 		Drupal.main.toggleMobile(context);
+		 Drupal.main.addClassMenuHeader(context);
 	 },
   };
-
+	Drupal.main.addClassMenuHeader = function (context) {
+		if ($('#block-lms-theme-account-menu').length > 0) {
+			if ($('[data-drupal-link-system-path="user/logout"]').length >0) {
+				$('[data-drupal-link-system-path="user/logout"]').addClass('user-logout');
+			}
+			if ($('[data-drupal-link-system-path="user/loin"]').length >0) {
+				$('[data-drupal-link-system-path="user/login"]').addClass('user-login');
+			}
+		}
+	}
   Drupal.main.toggleMobile = function (context) {
 	 const $header = $(once('MobileMenuToggle', '.header', context));
 	 if ($header.length > 0) {
